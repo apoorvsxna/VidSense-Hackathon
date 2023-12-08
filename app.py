f8d300a798c8eb6c9132e5469affbc0ebc2430c6
Apoorv Saxena <112375644+apoorvsxna@users.noreply.github.com>
2023-12-09 01:18:44 +0530

    Added updated version of all files.

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+from youtube_transcript_api import YouTubeTranscriptApi
+from transformers import pipeline
+
+app = Flask(__name__)
+
+@app.get('/answer')
+def answer_api():
+    url = request.args.get('url', '')
+    question = request.args.get('question', '')
+    video_id = url.split('=')[1]
+    answer = get_answer(get_transcript(video_id), question)
+    return answer, 200
+
+def get_transcript(video_id):
+    transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
+    transcript = ' '.join([d['text'] for d in transcript_list])
+    return transcript
+
+def get_answer(transcript, question):
+    qa_pipeline = pipeline('question-answering', model='bert-large-uncased-whole-word-masking-finetuned-squad')
+    answer = qa_pipeline(question=question, context=transcript)
+    return answer['answer']
+
+if __name__ == '__main__':
+    app.run()
